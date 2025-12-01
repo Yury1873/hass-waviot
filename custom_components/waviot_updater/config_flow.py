e0ecc77eeef5a886f1da7b2b185353b8529da074
Yuryk1873 <yury.kereynik@gmail.com>
2025-12-01 16:46:42 +0300

    Update README.md

--- custom_components/waviot_updater/config_flow.py
+++ custom_components/waviot_updater/config_flow.py
@@ -1,16 +1,16 @@
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_API_KEY, CONF_MODEM_ID
+from . import const
 import voluptuous as vol
 
-class WaviotFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class WaviotFlowHandler(config_entries.ConfigFlow, domain=  const.DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title=f"Modem {user_input[CONF_MODEM_ID]}", data=user_input)
+            return self.async_create_entry(title=f"Modem {user_input[const.CONF_MODEM_ID]}", data=user_input)
 
         schema = vol.Schema({
-            vol.Required(CONF_API_KEY): str,
-            vol.Required(CONF_MODEM_ID): str,
+            vol.Required(const.CONF_API_KEY): str,
+            vol.Required(const.CONF_MODEM_ID): str,
         })
         return self.async_show_form(step_id="user", data_schema=schema)
